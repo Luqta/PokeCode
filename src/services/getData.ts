d8633c102e40183eee35d9de4272dcea8b4b838e
Lucas Mateus Ferreira Lima da Silva <lucas.silva@class-solutions.com.br>
2022-02-11 11:37:24 -0300

    Consertado problema na requisição

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -2,25 +2,22 @@ import axios from "axios";
 import { IPokemon } from "../types/IPokemon";
 
 export const getData = async () => {
-    let arr:IPokemon[] = []
-    const allPokemons = await fetchData("https://pokeapi.co/api/v2/pokemon?limit=5")
+    const arr:IPokemon[] = []
+    const allPokemons = await fetchData("https://pokeapi.co/api/v2/pokemon?limit=10")
 
-    const teste = allPokemons.results.map(
-        async (
-            pokemon: { 
-                name: string, 
-                url: string 
-            }
-        ) => {
-            const pokeInfo = await fetchData(pokemon.url)
-            return {id: pokeInfo.id, nome: pokeInfo.name}
+    const pokemonsDetalhados = await Promise.all(allPokemons.results.map(async (
+        pokemon: { 
+            name: string, 
+            url: string 
         }
+    ) => {
+        const pokeInfo = await fetchData(pokemon.url)
+        return {id: pokeInfo.id, nome: pokeInfo.name}
+    })
     )
-    
-    console.log(teste)
-    console.log(arr)
 
-    return arr
+  
+    return pokemonsDetalhados
 };
 
 const fetchData = async (url: string) => {
